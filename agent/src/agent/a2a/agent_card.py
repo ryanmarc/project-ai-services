c2a0aa8a9eb2ec63075c7a00eb30b816aebe087e
Ryan Edgell <redgell@us.ibm.com>
2026-03-26 11:10:18 -0500

    Updates the a2a-sdk package info
    Signed-off-by: Ryan Edgell <redgell@us.ibm.com>

--- agent/src/agent/a2a/agent_card.py
+++ agent/src/agent/a2a/agent_card.py
@@ -1,6 +1,6 @@
 """Agent Card definition for A2A protocol discovery."""
 
-from a2a.types import AgentCard, AgentCapabilities, AgentSkill
+from a2a_sdk.types import AgentCard, AgentCapabilities, AgentSkill
 
 
 def build_agent_card(host: str = "0.0.0.0", port: int = 9000) -> AgentCard:
